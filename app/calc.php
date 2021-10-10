936a34a9bf96c605eda3916f18d39698635d66fa
Patryk_Kulpinski <patrykakakulpa399@gmail.com>
2021-10-10 11:22:45 +0200

    Php_02

--- app/calc.php
+++ app/calc.php
@@ -1,27 +1,32 @@
 <?php
 require_once dirname(__FILE__).'/../config.php';
 
-$x = $_REQUEST ['x'];
-$y = $_REQUEST ['y'];
-$op = $_REQUEST ['op'];
+include _ROOT_PATH.'/app/security/check.php';
 
-
-if ( ! (isset($x) && isset($y) && isset($op))) {
-	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+function getParams(&$x,&$y,&$op){
+	$x = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$y = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$op = isset($_REQUEST['op']) ? $_REQUEST['op'] : null;	
 }
 
-if ( $x == "") {
-	$messages [] = 'Nie podano Kwoty Kredytu';
-}
-if ( $y == "") {
-	$messages [] = 'Nie podano Czasu trwania';
-}
-if ( $op == "") {
-	$messages [] = 'Nie podano Oprocentowania';
-}
+function validate(&$x,&$y,&$op,&$messages){
+	if ( ! (isset($x) && isset($y) && isset($op))) {
+		return false;
+	}
 
+	if ( $x == "") {
+		$messages [] = 'Nie podano Kwoty Kredytu';
+	}
+	if ( $y == "") {
+		$messages [] = 'Nie podano Czasu trwania';
+	}
+	if ( $op == "") {
+		$messages [] = 'Nie podano Oprocentowania';
+	}
+	
+	
 
-if (empty( $messages )) {
+	if (count ( $messages ) != 0) return false;
 	
 	if (! is_numeric( $x )) {
 		$messages [] = 'Wartość Kredytu nie jest liczbą całkowitą';
@@ -33,21 +38,37 @@ if (empty( $messages )) {
 
 	if (! is_numeric( $op )) {
 		$messages [] = 'Oprocentowanie nie jest liczbą';
-	}		
+	}			
+	
 
+	if (count ( $messages ) != 0) return false;
+	else return true;
 }
 
-
-if (empty ( $messages )) {
+function process(&$x,&$y,&$op,&$messages,&$result){
+	global $role;
 	
 	$x = intval($x);
 	$y = intval($y);
 	$op = doubleval($op);
 	
-	$result = (($x + (($x * $op)/100)) / ($y*12));
+	$result = (($x + (($x * $op) / 100)) / ($y*12));
 
 	$result = number_format($result,2,',',' ');
-
 }
+
+
+$x = null;
+$y = null;
+$op = null;
+$result = null;
+$messages = array();
+
+
+getParams($x,$y,$op);
+if ( validate($x,$y,$op,$messages) ) { 
+	process($x,$y,$op,$messages,$result);
+}
+
 
 include 'calc_view.php';
